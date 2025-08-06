041cd08f9abe9035ba5fb75b588609ffbab341f6
SenukDev <senuk.jay@gmail.com>
2025-08-06 17:55:48 +1000

    Created Basic Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
+import Education from "./components/Education";
+import Introduction from "./components/Introduction";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import WorkExperience from "./components/WorkExperience";
 
 export default function Home() {
-  return (
-    <main><h1>Hello World</h1></main>
-  );
+    return (
+        <main>
+            <Introduction/>
+            <Skills/>
+            <Education/>
+            <Projects/>
+            <WorkExperience/>
+        </main>
+    );
 }
